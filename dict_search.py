5b7f93c0efb60ca355e75db0402f4d473fffac52
wreid <Williamwreid@gmail.com>
2014-06-24 14:06:19 -0400

    Added imports and main, and started parse_num.

--- dict_search.py
+++ dict_search.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env
 """
 Script for Jacunski exercise 5. 
 """
+import csv
+from sys import argv
+from sys import exit
+from os.path import exists
+
+def main(arguments):
+
+    input_name, output_name, terms_file, in_number = arguments[1:5]
+
+
+
+if __name__ == '__main__':
+    main(argv)
+
+def parse_num(num):
+    for i in range(0, len(num)):
+        try:
+            a = int(num[i])
+        except TypeError:
+            print 'Final argument must include all integer characters.'
+            exit()
