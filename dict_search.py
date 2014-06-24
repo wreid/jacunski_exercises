ba4726260dfff45f4bb9cf408b116e448bc7d5e1
wreid <Williamwreid@gmail.com>
2014-06-24 14:13:12 -0400

    Finished parse_num.

--- dict_search.py
+++ dict_search.py
@@ -10,16 +10,22 @@ from os.path import exists
 def main(arguments):
 
     input_name, output_name, terms_file, in_number = arguments[1:5]
-
-
-
-if __name__ == '__main__':
-    main(argv)
+    header_length, fields = parse_num(in_number)
+    print header_length
+    print fields
 
 def parse_num(num):
+    a = []
     for i in range(0, len(num)):
         try:
-            a = int(num[i])
+            a.append(int(num[i]))
         except TypeError:
             print 'Final argument must include all integer characters.'
             exit()
+
+    hl = a[0]
+    fs = a[1:]
+    return hl, fs
+
+if __name__ == '__main__':
+    main(argv)
